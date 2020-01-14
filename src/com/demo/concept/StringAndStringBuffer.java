da8bf93265d3a5c1f4d101deb5404e18a4272a0f
sunita09paul <paulsunita26@gmail.com>
2020-01-14 18:38:09 +0530

    new des

--- src/com/demo/concept/StringAndStringBuffer.java
+++ src/com/demo/concept/StringAndStringBuffer.java
@@ -7,6 +7,7 @@ public class StringAndStringBuffer {
 		String s = new String ("Sunita");
 		s.concat("Paul");
 		System.out.println(s);
+		System.out.println("I am Rushikesh");
 		
 		
 		StringBuffer p =new StringBuffer("Sunita");
